6ff046e3b804fdba728dd8832e7c29b0493455b6
akshay <akshay@akshay-PC>
2022-05-07 17:09:00 +0530

    Firsht Commit

---
+++ HelloWorld/src/mypack/Helloworld.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hellow I am Pranjal and Its my !st java Code");
+	}
+
+}
